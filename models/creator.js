a3086c11d67a4c676b138d2a8491a07f9aad4fcb
neilaybhatt27 <neilaybhatt27@gmail.com>
2023-01-28 12:00:24 +0530

    PassportJS and Google OAuth initialized

--- models/creator.js
+++ models/creator.js
@@ -48,3 +48,36 @@ const creatorSchema = new mongoose.Schema({
         }
     }
 });
+
+creatorSchema.plugin(passportLocalMongoose);
+creatorSchema.plugin(findOrCreate);
+
+const Creator = mongoose.model("Creator", creatorSchema);
+
+passport.use(Creator.createStrategy());
+
+passport.serializeUser(function(user,done){
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+    Creator.findById(id, function(err, user){
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/google/",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    Creator.findOrCreate({ googleId: profile.id }, function (err, user) {
+        console.log(profile);
+      return cb(err, user);
+    });
+  }
+));
+
+module.exports = Creator;
